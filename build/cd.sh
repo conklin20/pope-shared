c8f6a512caba16c691fceaf2a3a1ae982f36189e
Cary Conklin <cary.conklin.20@gmail.com>
2022-01-26 20:02:04 -0600

    comment out access key

--- build/cd.sh
+++ build/cd.sh
@@ -15,8 +15,8 @@ docker build -f ./src/Shared/Dockerfile \
     -t pope-shared-container .
 
 docker run --rm \
-    -e AWS_ACCESS_KEY_ID="AKIA5TUAS74C3GQ5GEPA" \
-    -e AWS_SECRET_ACCESS_KEY="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" \
+    -e AWS_ACCESS_KEY_ID="" \
+    -e AWS_SECRET_ACCESS_KEY="" \
     -e AWS_DEFAULT_REGION="us-east-1" \
     --name release-pope-shared pope-shared-container \
     --source "https://c2software-935498809093.d.codeartifact.us-east-1.amazonaws.com/nuget/pope-shared-repo/v3/index.json"
--- build/publish-nuget.sh
+++ build/publish-nuget.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-aws configure set aws_access_key_id "AKIA5TUAS74C3GQ5GEPA"
-aws configure set aws_secret_access_key "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"
+aws configure set aws_access_key_id ""
+aws configure set aws_secret_access_key ""
 aws configure set region "us-east-1"
 
 dotnet nuget push *.nupkg "$@"
